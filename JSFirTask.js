204138d01c791f23ed749b15892e84cc2b0ab29b
BalacaYusuf <xamoyev.royal1998@gmail.com>
2023-07-13 13:28:29 +0300

    commit

---
+++ JSFirTask.js
@@ -0,0 +1,107 @@
+
+// Task : 1
+
+// const person=prompt("Please enter your name.")
+// console.log(person);
+
+
+
+// Task : 2
+
+// const Num1=parseInt(prompt("Please enter Num1."));
+// const Num2=parseInt(prompt("Please enter Num2."));
+
+// console.log(Num1);
+// console.log(Num2);
+
+// console.log(typeof(Num1));
+// console.log(Num1);
+
+// function numbers(x,y)
+// {
+//     if(x===1 && y===2)
+
+//         return x+y;
+
+//         else  {
+//             console.log("Entered number is uncorrect");
+//         }
+    
+
+// }
+
+// console.log(numbers(Num1,Num2));
+
+
+// Task : 3 
+
+// const Num1=parseInt(prompt("Please enter Number greater than 100."));
+
+// console.log(`You entered : ${Num1}`);
+
+// while(Num1<100) {
+//     console.log(`You entered : ${Num1} and it is not greater than 100. Enter Again ,Please..`);
+//  const Num3=parseInt(prompt("Please enter Number greater than 100."));
+//      if(Num3>100){
+
+//         console.log(`You entered : ${Num3}`);
+//         break;
+//      }
+   
+// }
+
+// Task : 4
+
+// const Num=parseInt(prompt("Please enter Number ."));
+
+// if(Num>0){
+//     alert(`The number you entered number : ${ Num} is greater than 0`);
+// }
+// else if (Num==0){
+//     alert(`The number you entered number : ${ Num} is equal to 0`);
+// }
+// else if (Num<0){
+//     alert(`The number you entered number : ${ Num} is less than 0`);
+// }
+
+
+// Task : 5
+
+// const login=prompt("Eneter your login.Please.");
+
+// login='Employee' ? login='Director' ? login==null ? 
+// alert("Hello") : alert("Greetings") : alert("No Login") : alert ("?");
+
+// Task 6 :
+
+
+
+
+const Num1=parseInt(prompt("Please enter Num1."));
+const Num2=parseInt(prompt("Please enter Num2."));
+
+// 
+
+function numbers(x,y)
+
+{
+   
+ return x**y;
+
+}
+
+console.log(numbers(Num1,Num2));
+
+
+alert(`The number : ${Num1} with power : ${Num2} equal : ${numbers(Num1,Num2)}`);
+
+
+
+
+
+
+
+
+
+
+
